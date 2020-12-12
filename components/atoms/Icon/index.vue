6ad0c9f88981a79b372fb8f3417e7a0678fb29a3
Peter Kim <peterkimzz69@gmail.com>
2020-12-12 16:24:26 +0900

    icon

---
+++ components/atoms/Icon/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <component :is="Icon" class="icon" @click="Click" />
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      default: null
+    }
+  },
+  computed: {
+    Icon() {
+      try {
+        return () => import(`./${this.icon}.vue`)
+      } catch (err) {
+        return ''
+      }
+    }
+  },
+  methods: {
+    Click() {
+      console.log('')
+      this.$emit('click')
+    }
+  }
+}
+</script>
+
+<style lang="postcss" scoped>
+.icon {
+  @apply inline-block;
+  @apply w-5;
+}
+</style>
---
+++ components/atoms/Link/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <component
+    :is="tag"
+    :href="href"
+    :to="href"
+    :target="target"
+    :class="['link', { block }]"
+    ><slot
+  /></component>
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      default: '',
+      required: true
+    },
+    target: {
+      type: String,
+      default: '_self'
+    },
+    block: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    tag() {
+      if (this.target === '_blank') {
+        return 'a'
+      }
+      return 'n-link'
+    }
+  }
+}
+</script>
+
+<style lang="postcss" scoped>
+.link {
+  @apply inline-block;
+  @apply hover:underline;
+}
+</style>
