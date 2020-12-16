8f5e35da69ff4561507bc26173f7a8200e351aa0
Peter Kim <peterkimzz69@gmail.com>
2020-12-16 14:02:21 +0900

    updated & kofi

---
+++ components/molecules/SponsorButton/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="text-xs md:text-sm shadow-sm text-gray-500 font-medium bg-white border border-gray-200 rounded-md p-4"
+  >
+    <div class="flex">
+      <div class="mr-2">☕</div>
+      <div>
+        <p>
+          제 포스팅이 도움이 되셨다면, 아래 버튼을 눌러 <b>커피 한 잔</b>을
+          후원해주세요! 더 좋은 글을 발행하는데 큰 도움이 됩니다.
+        </p>
+        <a
+          href="https://ko-fi.com/U7U22YFKV"
+          target="_blank"
+          class="inline-block mt-4 bg-white shadow-sm overflow-hidden"
+          ><img
+            class="w-36"
+            src="/ko-fi.png"
+            alt="Buy Me a Coffee at ko-fi.com"
+          />
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
