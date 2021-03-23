94ed3c36f9a8e16b55ba24603d47013406ee6f0c
Peter Kim <peterkimzz69@gmail.com>
2021-03-23 20:00:03 +0900

    jit error

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -18,6 +18,25 @@
       </div>
     </div>
 
+    <div class="flex my-4 bg-cyan-700 p-2 rounded-lg">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        class="w-7 h-7 text-cyan-400"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+          clip-rule="evenodd"
+        />
+      </svg>
+      <div class="flex-1 ml-2 text-sm leading-6 text-cyan-50">
+        현재 <code>@tailwindcss/jit</code> 모듈에서 발생하는 이슈로 인해 포스팅
+        내 텍스트의 스타일 적용이 안됩니다. 최대한 빠르게 해결하도록 하겠습니다.
+      </div>
+    </div>
+
     <article>
       <!-- <vue-info-box>
           <template #info-box>
