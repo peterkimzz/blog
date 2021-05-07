72da32f6394dad2165941500a83f61025ae66de0
peterkimzz <peterkimzz69@gmail.com>
2021-05-07 05:02:09 +0000

    Deploying to gh-pages from  @ bc9dbb990dfdf0ca2179db20ec362515c5a7151b 🚀

---
+++ _nuxt/static/1620363723/resume/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620363723",layout:"article",error:a,serverRendered:true,routePath:"\u002Fresume",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9654c29c"}}}}(null));
