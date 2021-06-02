58870d9f3ac2a393f4e89a6e6198f6598643755c
peterkimzz <peterkimzz69@gmail.com>
2021-06-02 07:44:35 +0000

    Deploying to gh-pages from @ peterkimzz/blog@8e604a319e7501df98a25c49f95d376739d8b676 🚀

---
+++ _nuxt/static/1622619872/how-to-pick-web-font-and-size/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622619872",layout:"article",error:a,serverRendered:true,routePath:"\u002Fhow-to-pick-web-font-and-size",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e29b3400"}}}}(null));
