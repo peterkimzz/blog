2419a72909001764675339e0acf8071bf11268d5
peterkimzz <peterkimzz69@gmail.com>
2021-07-05 04:07:05 +0000

    Deploying to gh-pages from @ peterkimzz/blog@addc2b84626610f1af25e181045d5b582f851e17 🚀

---
+++ _nuxt/static/1625458022/memoirs-by-2020/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1625458022",layout:"article",error:a,serverRendered:true,routePath:"\u002Fmemoirs-by-2020",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"777e10c8"}}}}(null));
