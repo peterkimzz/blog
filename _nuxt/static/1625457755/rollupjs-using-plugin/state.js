43a518e379d0f231acc10aa5e72e1120254c110d
peterkimzz <peterkimzz69@gmail.com>
2021-07-05 04:02:37 +0000

    Deploying to gh-pages from @ peterkimzz/blog@c06174ab29e997ae49bf72daaaf9579b45b782a8 🚀

---
+++ _nuxt/static/1625457755/rollupjs-using-plugin/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1625457755",layout:"article",error:a,serverRendered:true,routePath:"\u002Frollupjs-using-plugin",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8727b32e"}}}}(null));
