b57a635c87b177f267b065ee22ce3ed0d2b22800
peterkimzz <peterkimzz69@gmail.com>
2021-09-04 09:02:50 +0000

    Deploying to gh-pages from @ peterkimzz/blog@ab991bc61c9ea54188648f15990b3e0feb6e9cf7 🚀

---
+++ _nuxt/static/1630746167/websocket-vs-socket-io/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630746167",layout:"article",error:a,serverRendered:true,routePath:"\u002Fwebsocket-vs-socket-io",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bf243024"}}}}(null));
