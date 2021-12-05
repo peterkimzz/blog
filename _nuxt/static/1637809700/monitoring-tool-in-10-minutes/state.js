828ceedc7b8c25f697ce94dbafe994bcee727aaa
peterkimzz <peterkimzz69@gmail.com>
2021-12-05 13:28:40 +0000

    Deploying to gh-pages from @ peterkimzz/blog@e04825dfac7173b25d32ed4839d526816a4f657b 🚀

--- _nuxt/static/1637809700/monitoring-tool-in-10-minutes/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637809700",layout:"article",error:a,serverRendered:true,routePath:"\u002Fmonitoring-tool-in-10-minutes",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1fb639e4"}}}}(null));
