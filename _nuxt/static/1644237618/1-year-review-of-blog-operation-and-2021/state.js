519545aa1393d5bff0bc6a4269ff750354359848
peterkimzz <peterkimzz69@gmail.com>
2022-02-07 12:40:23 +0000

    Deploying to gh-pages from @ peterkimzz/blog@412b38f4edfe85b24e3b45efdfbdc936d2508060 🚀

---
+++ _nuxt/static/1644237618/1-year-review-of-blog-operation-and-2021/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644237618",layout:"article",error:a,serverRendered:true,routePath:"\u002F1-year-review-of-blog-operation-and-2021",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a50e7703"}}}}(null));
