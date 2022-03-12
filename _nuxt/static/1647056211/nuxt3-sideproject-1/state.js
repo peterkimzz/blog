f12b7697e6ec43aedbfd19c028d85e738f53975c
peterkimzz <peterkimzz69@gmail.com>
2022-03-12 08:33:38 +0000

    Deploying to gh-pages from @ peterkimzz/blog@9c5abfc3f6df2a25857d7870b97220c5c870bcd1 🚀

--- _nuxt/static/1647056211/nuxt3-sideproject-1/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647056211",layout:"article",error:a,serverRendered:true,routePath:"\u002Fnuxt3-sideproject-1",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f37ed2b9"}}}}(null));
---
+++ _nuxt/static/1647074013/cryptocurrency-price-in-a-second/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647074013",layout:"article",error:a,serverRendered:true,routePath:"\u002Fcryptocurrency-price-in-a-second",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ad5d4bc9"}}}}(null));
