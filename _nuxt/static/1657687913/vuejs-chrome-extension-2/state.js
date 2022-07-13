2c3f43bd3c0451922621a2982a181f0dc8918fe5
peterkimzz <peterkimzz69@gmail.com>
2022-07-13 04:51:58 +0000

    Deploying to gh-pages from @ peterkimzz/blog@e55cb39e766a75a1bc1828e95854bb850618a9f2 🚀

---
+++ _nuxt/static/1657687913/vuejs-chrome-extension-2/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1657687913",layout:"article",error:a,serverRendered:true,routePath:"\u002Fvuejs-chrome-extension-2",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8ef83836"}}}}(null));
