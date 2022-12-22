28fe50c196896b24617ac379c060b559475d029f
peterkimzz <peterkimzz69@gmail.com>
2022-12-22 13:27:46 +0000

    Deploying to gh-pages from @ peterkimzz/blog@e28edde3cedabd9eb1824d17a2e5557b7c77555f 🚀

--- _nuxt/static/1657687913/introduce-free-responsive-email-template-mjml/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1657687913",layout:"article",error:a,serverRendered:true,routePath:"\u002Fintroduce-free-responsive-email-template-mjml",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8ef83836"}}}}(null));
