8bba0dee1c5f3ee52d57d72a876730211f952841
Peter Donghyun Kim <peterkimzz69@gmail.com>
2022-12-25 13:49:20 +0900

    max-w-prose

--- layouts/article.vue
+++ layouts/article.vue
@@ -1,15 +1,15 @@
 <template>
   <div id="article__layout">
-    <vue-container class="max-w-screen-md">
+    <vue-container class="max-w-prose">
       <vue-navigation-bar />
     </vue-container>
 
-    <vue-container class="max-w-screen-md">
+    <vue-container class="max-w-prose">
       <Nuxt />
     </vue-container>
 
     <div class="bg-gray-900">
-      <vue-container class="max-w-screen-md">
+      <vue-container class="max-w-prose">
         <vue-footer-bar />
       </vue-container>
     </div>
