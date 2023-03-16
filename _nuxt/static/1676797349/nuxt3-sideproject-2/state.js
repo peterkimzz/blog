d489ee59f5481e35054c2fa6f29b1565dda4c115
peterkimzz <peterkimzz69@gmail.com>
2023-03-16 08:35:04 +0000

    Deploying to gh-pages from @ peterkimzz/blog@1b1d646c236c8c8378da3f5b3c94edd06a003959 🚀

--- _nuxt/static/1676797349/nuxt3-sideproject-2/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1676797349",layout:"article",error:a,serverRendered:true,routePath:"\u002Fnuxt3-sideproject-2",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7d558f74"}}}}(null));
---
+++ _nuxt/static/1678955699/introduce-free-responsive-email-template-mjml/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1678955699",layout:"article",error:a,serverRendered:true,routePath:"\u002Fintroduce-free-responsive-email-template-mjml",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"394871af"}}}}(null));
